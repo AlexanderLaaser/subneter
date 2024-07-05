4d83a3f7c69daa40c56b16ac65d9a3e704f24bc4
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-07-05 22:48:04 +0200

    changed client id to normal id

--- infrastructure/terraform/acr.tf
+++ infrastructure/terraform/acr.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_role_assignment" "containerappsacrpull" {
-  principal_id                     = azurerm_user_assigned_identity.main.client_id
+  principal_id                     = azurerm_user_assigned_identity.main.id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
