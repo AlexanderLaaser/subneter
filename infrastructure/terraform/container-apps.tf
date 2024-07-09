992d7a7a283e2cd11d1b5a2042cd03f80fb99324
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-07-10 01:24:11 +0200

    d

--- infrastructure/terraform/container-apps.tf
+++ infrastructure/terraform/container-apps.tf
@@ -134,7 +134,7 @@ resource "azurerm_container_app" "django" {
 
       env {
         name  = "VITE_BACKEND_URL"
-        value = var.VITE_BACKEND_URL
+        value = "subneter-django.internal.graysand-3f812d09.westeurope.azurecontainerapps.io"
       }
     }
   }
