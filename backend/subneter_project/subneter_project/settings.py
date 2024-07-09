9bb07cafa4a904cde68447c6754ab6b339d0e787
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-07-09 12:44:15 +0200

    allowed hosts

--- backend/subneter_project/subneter_project/settings.py
+++ backend/subneter_project/subneter_project/settings.py
@@ -23,7 +23,7 @@ BACKEND_URL = config('BACKEND_URL', default='')
 print(f"Configured FRONTEND_URL: {FRONTEND_URL}")
 print(f"Configured BACKEND_URL: {BACKEND_URL}")
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'subneter.de',"https://subneter-django.gentlebeach-face0caa.westeurope.azurecontainerapps.io"]
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'subneter.de','subneter-django.gentlebeach-face0caa.westeurope.azurecontainerapps.io']
 
 print(f"Configured ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
